113eb1c520972d93483d3412987abca15add5034
terefet <tutg24@gmail.com>
2021-12-18 11:01:03 +0300

    dynamic value in template

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "welcme to the new blog"
   return (
     <div className="App">
       <div className="content">
         <h1>App Commponent</h1>
         <h2>hi there</h2>
+        <h3>{title}</h3>
       </div>
     </div>
   );
