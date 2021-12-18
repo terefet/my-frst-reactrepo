b02d1b87f169e001c07d6b02b0d59e48be1ada56
terefet <tutg24@gmail.com>
2021-12-18 11:56:33 +0300

    message

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () => {
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/crate">New Blog</a>
+                <h2>merge cheak up1</h2>
             </div>
         </nav>
      );
