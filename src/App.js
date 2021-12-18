6eff1de02e638515f1e56561802a332ecc69c8da
terefet <tutg24@gmail.com>
2021-12-18 11:22:38 +0300

    insrted link

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   const title = "welcme to the new blog"
+  const link = 'http://www.google.com';
   return (
     <div className="App">
       <div className="content">
         <h1>App Commponent</h1>
         <h2>Terefe comming home soon</h2>
         <h3>{title}</h3>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
