078f8427cf79af7a4cbda23d1b3013f5215fc83f
terefet <tutg24@gmail.com>
2021-12-20 10:42:09 +0300

    event clicks

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = "welcme to the new blog"
-  const link = 'http://www.google.com';
+  
   return (
     <div className="App">
       <Navbar />
